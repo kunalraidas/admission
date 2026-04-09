6c777ecd521337323a9533c500289e4b4875401e
Kunal Raidas <96783461+kunalraidas@users.noreply.github.com>
2026-04-09 10:50:36 +0530

    sendFolloUpEmail is Updated.

--- src/main/java/com/kunal/admission/service/EmailService.java
+++ src/main/java/com/kunal/admission/service/EmailService.java
@@ -28,9 +28,19 @@ public class EmailService {
         message.setText(
                 "Hello " + visitor.getName() + ",\n\n" +
                         "We noticed that you started your admission process.\n" +
+                        "Thank you for your interest in BVICAM!\n\n" +
+                        "Here are the courses we offer:\n\n" +
+                        "1. BCA (Bachelor of Computer Applications) - 3 Years\n" +
+                        "2. MCA (Master of Computer Applications) - 2 Years\n" +
+                        "3. B.Tech (Computer Science & Engineering) - 4 Years\n" +
+                        "4. MBA (Master of Business Administration) - 2 Years\n\n" +
+                        "For admission details, fee structure, and eligibility,\n" +
+                        "please visit: https://www.bvicam.ac.in\n\n" +
+                        "Or contact our admission office at: admissions@bvicam.ac.in\n\n" +
                         "Please update your admission status at the earliest.\n\n" +
                         "Regards,\n" +
                         "BVICAM Admission Team"
+
         );
         mailSender.send(message);
         log.info("Follow - Up email sent to {}", visitor.getEmail());
